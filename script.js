9d0d6851338f612853549da26646e134a7a7662b
Dez <dezd24@gmail.com>
2022-10-23 11:54:10 -0700

    i need cookies

---
+++ script.js
@@ -0,0 +1,13 @@
+var headerEl = document.querySelector('header');
+var startButtEl = document.querySelector('#startButt');
+var mainHead = document.querySelector('#mainHead');
+var mainP = document.querySelector('#mainP');
+var timerEl = document.querySelector('#timer');
+var pageContentEl = document.querySelector('#pageContent');
+var highScoreButtEl = document.querySelector('#highScoreButt');
+var questionCount = 0;
+var questionNum = {}
+
+var timeLeft = 0
+timerEl.textContent = "Time: " + timeLeft;
+
